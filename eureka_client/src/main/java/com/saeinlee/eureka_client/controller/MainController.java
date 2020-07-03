e73a67e07869883eaf347b45f49a95ed69c2d36a
saein-lee <saeinlee@mytalk.co.kr>
2020-07-03 18:42:03 +0900

    client controller 예제 추가

--- eureka_client/src/main/java/com/saeinlee/eureka_client/controller/MainController.java
+++ eureka_client/src/main/java/com/saeinlee/eureka_client/controller/MainController.java
@@ -1,10 +1,13 @@
 package com.saeinlee.eureka_client.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MainController {
@@ -13,7 +16,10 @@ public class MainController {
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/ping")
-    public ResponseEntity<String> mainPing() {
-        return ResponseEntity.ok("pong");
+    public ResponseEntity<List<ServiceInstance>> mainPing() {
+        List<ServiceInstance> instanceList = discoveryClient.getInstances("service-1");
+        System.out.println("INSTANCES: count = " + instanceList.size());
+        instanceList.forEach(it->System.out.println("INSTANCES: id=" + it.getServiceId() + "port= " + it.getPort()));
+        return ResponseEntity.ok(instanceList);
     }
 }
