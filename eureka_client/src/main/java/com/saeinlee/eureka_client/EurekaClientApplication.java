03b5d956a492ee8384f41752d5fe2275b6a1b917
saeinlee <saeinlee@218.38.137.27>
2020-07-07 00:18:58 +0900

    설정변

--- eureka_client/src/main/java/com/saeinlee/eureka_client/EurekaClientApplication.java
+++ eureka_client/src/main/java/com/saeinlee/eureka_client/EurekaClientApplication.java
@@ -3,10 +3,15 @@ package com.saeinlee.eureka_client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class EurekaClientApplication {
+	
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
